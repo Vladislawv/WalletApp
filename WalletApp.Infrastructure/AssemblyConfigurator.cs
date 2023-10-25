ca285dfc5afa9c35012c6162ced2bd1306cb0e8c
v.yusupov <v.yusupov@upswot.com>
2023-10-25 22:44:19 +0300

    (feat): Set up ef core using PostgreSql as database; (feat): Implemented auto-migration while starting the api server; (feat): Moved database connection string to the user-secrets.

---
+++ WalletApp.Infrastructure/AssemblyConfigurator.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using WalletApp.Infrastructure.DataAccess.Database;
+
+namespace WalletApp.Infrastructure;
+
+public static class AssemblyConfigurator
+{
+    private const string WALLET_APP_POSTGRE_SQL_SERVER = "WalletAppPostgreSqlServer";
+    
+    public static IServiceCollection ConfigureInfrastructureServices(
+        this IServiceCollection services, IConfiguration configuration)
+    {
+        services.AddDbContext<WalletAppContext>(options =>
+            options.UseNpgsql(configuration.GetConnectionString(WALLET_APP_POSTGRE_SQL_SERVER)));
+
+        return services;
+    }
+
+    public static IServiceProvider InitializeDatabase(this IServiceProvider services)
+    {
+        using var scope = services.CreateScope();
+        var walletAppContext = scope.ServiceProvider.GetRequiredService<WalletAppContext>();
+        walletAppContext.Database.Migrate();
+
+        return services;
+    }
+}
