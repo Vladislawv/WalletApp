d59b4a13b7ff0f2c48fa2a57801c4ae0b485317a
v.yusupov <v.yusupov@upswot.com>
2023-10-27 13:21:50 +0300

    (feat): Created UserController with get method by id.

---
+++ Server/WalletApp.Api/Controllers/User/Dto/Convertor.cs
@@ -0,0 +1,16 @@
+﻿using UserDomain = WalletApp.Domain.UserAggregate.User;
+
+namespace WalletApp.Api.Controllers.User.Dto;
+
+public static class Convertor
+{
+    public static UserDto ToDto(this UserDomain user)
+    {
+        return new UserDto
+        {
+            Id = user.Id,
+            Name = user.UserName,
+            DailyPoints = user.DailyPoints
+        };
+    }
+}
---
+++ Server/WalletApp.Api/Controllers/User/Dto/UserDto.cs
@@ -0,0 +1,8 @@
+﻿namespace WalletApp.Api.Controllers.User.Dto;
+
+public class UserDto
+{
+    public Guid Id { get; set; }
+    public string Name { get; set; }
+    public string DailyPoints { get; set; }
+}
