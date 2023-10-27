7cd0b32dc14902497567f5a72a4a1de8b7a80252
v.yusupov <v.yusupov@upswot.com>
2023-10-27 13:31:43 +0300

    (feat): Implemented JWT Auth in swagger.

--- Server/WalletApp.Api/AssemblyConfigurator.cs
+++ Server/WalletApp.Api/AssemblyConfigurator.cs
@@ -12,10 +12,7 @@ public static class AssemblyConfigurator
     {
         services
             .AddEndpointsApiExplorer()
-            .AddSwaggerGen(options =>
-            {
-                options.SwaggerDoc("v1", new OpenApiInfo { Title = "WalletApp API", Version = "v1" });
-            })
+            .AddSwaggerConfiguration()
             .AddControllers();
         
         services.AddTransient<TransactionIdMiddleware>();
@@ -41,5 +38,38 @@ public static class AssemblyConfigurator
         app.MapControllers();
 
         return app;
+    }
+
+    private static IServiceCollection AddSwaggerConfiguration(this IServiceCollection services)
+    {
+        services.AddSwaggerGen(options =>
+        {
+            options.SwaggerDoc("v1", new OpenApiInfo { Title = "WalletApp API", Version = "v1" });
+            options.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme
+            {
+                Scheme = "Bearer",
+                BearerFormat = "JWT",
+                In = ParameterLocation.Header,
+                Name = "Authorization",
+                Description = "Bearer Authentication with JWT Token",
+                Type = SecuritySchemeType.Http
+            });
+            options.AddSecurityRequirement(new OpenApiSecurityRequirement
+            {
+                {
+                    new OpenApiSecurityScheme
+                    {
+                        Reference = new OpenApiReference
+                        {
+                            Id = "Bearer",
+                            Type = ReferenceType.SecurityScheme
+                        }
+                    },
+                    new List<string>()
+                }
+            });
+        });
+
+        return services;
     }
 }
