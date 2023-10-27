1ca84f5f90847b189a7a2a7d9f47f1dfc6cc4108
v.yusupov <v.yusupov@upswot.com>
2023-10-27 16:49:07 +0300

    (feat): Implemented card creation with 10 transactions generation.

---
+++ Server/WalletApp.Application/CommandHandlers/CreateCardCommandHandler.cs
@@ -0,0 +1,55 @@
+﻿using Shared.InternalMessaging.CQRS.Abstractions;
+using WalletApp.Application.Commands;
+using WalletApp.Domain.CardAggregate;
+using WalletApp.Domain.TransactionAggregate;
+
+namespace WalletApp.Application.CommandHandlers;
+
+public class CreateCardCommandHandler : ICommandHandler<CreateCardCommand>
+{
+    private const int MAX_TOTAL = 1500;
+    private const bool IS_PAYMENT_REQUIRED = false;
+    
+    private readonly ICardRepository _cardRepository;
+    private readonly ITransactionService _transactionService;
+
+    public CreateCardCommandHandler(ICardRepository cardRepository, ITransactionService transactionService)
+    {
+        _cardRepository = cardRepository;
+        _transactionService = transactionService;
+    }
+
+    public async Task Handle(CreateCardCommand request, CancellationToken cancellationToken)
+    {
+        cancellationToken.ThrowIfCancellationRequested();
+
+        var total = GenerateCardTotal();
+        
+        var card = new Card
+        {
+            UserId = request.UserId,
+            Total = total,
+            Available = MAX_TOTAL - total,
+            IsPaymentRequired = IS_PAYMENT_REQUIRED,
+            Description = GenerateDescription(),
+            Transactions = _transactionService.Generate(request.UserId).ToList()
+        };
+        
+        await _cardRepository.Cards.AddAsync(card, cancellationToken);
+        await _cardRepository.DbContext.SaveChangesAsync(cancellationToken);
+    }
+
+    private static double GenerateCardTotal()
+    {
+        return new Random().NextDouble() * MAX_TOTAL;
+    }
+
+    private static string GenerateDescription()
+    {
+        var currentMonth = DateTime.UtcNow.ToString("MMM");
+        
+        return !IS_PAYMENT_REQUIRED
+            ? string.Format("You've paid your {month} balance.", currentMonth)
+            : string.Format("You need to pay your {month} balance.", currentMonth);
+    }
+}
---
+++ Server/WalletApp.Application/Transactions/TransactionService.cs
@@ -0,0 +1,85 @@
+﻿using WalletApp.Domain.TransactionAggregate;
+
+namespace WalletApp.Application.Transactions;
+
+public class TransactionService : ITransactionService
+{
+    private const int DEFAULT_TRANSACTIONS_COUNT_TO_GENERATE = 10;
+    private const int MAX_TOTAL = 10000;
+
+    private static readonly Dictionary<string, string> TRANSACTIONS = new()
+    {
+        { "Apple", "Transaction from Apple" },
+        { "IKEA", "Transaction from IKEA" },
+        { "Target", "Transaction from Target" }
+    };
+
+
+    public IEnumerable<Transaction> Generate(Guid userId, int transactionsCount = 0)
+    {
+        transactionsCount = transactionsCount != 0 ? transactionsCount : DEFAULT_TRANSACTIONS_COUNT_TO_GENERATE;
+
+        for (var count = 0; count <= transactionsCount; count++)
+        {
+            var transactionType = GenerateTransactionType();
+            var transactionName = GetName(transactionType);
+            var creationDate = DateTime.UtcNow;
+            var isPending = GenerateIsPending();
+            
+            yield return new Transaction
+            {
+                UserId = userId,
+                // TODO IconId = null,
+                TransactionType = transactionType,
+                Total = GenerateTotal(),
+                Name = transactionName,
+                Description = GenerateDescription(isPending, transactionName),
+                CreatedOn = creationDate,
+                Date = GetDate(creationDate),
+                IsPending = isPending,
+            };
+        }
+    }
+
+    private static TransactionType GenerateTransactionType()
+    {
+        var randomType = new Random().Next(0, 2);
+        return (TransactionType)randomType;
+    }
+
+    private static double GenerateTotal()
+    {
+        return new Random().NextDouble() * MAX_TOTAL;
+    }
+
+    private static string GenerateDescription(bool isPending, string transactionName)
+    {
+        var description = TRANSACTIONS[transactionName]; 
+        return isPending ? $"Pending - {description}" : description;
+    }
+
+    private static string GetName(TransactionType transactionType)
+    {
+        return transactionType is TransactionType.Payment ? "Payment" : GenerateName();
+    }
+
+    private static string GenerateName()
+    {
+        var transactionNames = TRANSACTIONS.Keys.ToList();
+        
+        var randomIndex = new Random().Next(0, transactionNames.Count);
+        return transactionNames[randomIndex];
+    }
+
+    private static string GetDate(DateTime creationDate)
+    {
+        var isTransactionMadeInTheCurrentWeek = (DateTime.UtcNow - creationDate).Days < 7;
+        return isTransactionMadeInTheCurrentWeek ? creationDate.DayOfWeek.ToString() : creationDate.ToString("M/d/yy");
+    }
+
+    private static bool GenerateIsPending()
+    {
+        var randomIndex = new Random().Next(0, 2);
+        return randomIndex is not 0;
+    }
+}
